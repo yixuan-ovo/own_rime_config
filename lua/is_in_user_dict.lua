523ab7c71040833197a9e47108547cb4b3363370
10R4SLK\yixr <yixr>
2025-11-21 14:21:10 +0800

    修改emoji

--- lua/is_in_user_dict.lua
+++ lua/is_in_user_dict.lua
@@ -2,10 +2,7 @@
 -- is_in_user_dict: true           输入过的内容
 -- is_in_user_dict: false 或不写    未输入过的内容
 
-
-
 -- 根据是否在用户词典，追加标记符号
--- 📰 表示用户词典中；👻 表示整句联想
 
 local M = {}
 
@@ -20,14 +17,14 @@ function M.func(input, env)
   for cand in input:iter() do
     local comment = cand.comment or ""
 
-    -- ✅ 用户词典中，追加 ★
-    if cand.type == "user_phrase" and not comment:find("★") then
-      comment = comment .. "📰"
+    -- ✅ 用户词典中，追加 🌪
+    if cand.type == "user_phrase" and not comment:find("🌪") then
+      comment = comment .. "🌪"
     end
 
-    -- ✅ 整句联想，追加 ∞
-    if cand.type == "sentence" and not comment:find("∞") then
-      comment = comment .. "👻"
+    -- ✅ 整句联想，追加 💤
+    if cand.type == "sentence" and not comment:find("💤") then
+      comment = comment .. "💤"
     end
 
     cand.comment = comment
